7e50ba0596b9aeb4a3c568fba43ef2bf370ea0b2
Mallory <mallorydoescoding@gmail.com>
2021-10-01 18:25:50 -0500

    Base header created, font styling done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+//Imports
+import React from 'react';
+import Head from './components/Head';
+
+//Imports - Styles
 import './App.css';
 
 function App() {
   return (
     <div className="app-outer-wrap">
       <div className="app-inner-wrap">
-        
+        <Head />
       </div>
     </div>
   );
--- src/components/Head.js
+++ src/components/Head.js
@@ -6,12 +6,12 @@ const Head = () => {
         <div className="head-outer-wrap">
             <div className="head-inner-wrap">
                 <div className="head-title-address-wrap">
-                    <h1 className="head-title">Luna Bella</h1>
+                    <h1 className="head-title"><span className="luna">LUNA</span><span className="bella"> Bella</span></h1>
                     <p className="head-address">104 Grand Isle Way - Hot Springs, AR</p>
                 </div>
                 <div className="head-social-wrap">
-                    <a>Facebook</a>
-                    <a>Yelp</a>
+                    <span>Facebook</span>
+                    <span>Yelp</span>
                 </div>
             </div>
         </div>
